83d9a2c241ec01310965d83bbe57d0837d7a44c7
Michal Bojanowski <michal2992@gmail.com>
2014-08-26 01:20:26 +0200

    Added DESCRIPTION and package man page

---
+++ R/d3net-package.R
@@ -0,0 +1,8 @@
+#' Visualizing networks using d3js and Shiny
+#'
+#'Framework for creating Shiny applications for static and dynamic network
+#'visualizations using d3js	
+#'
+#' @name d3net.package
+#' @docType package
+NULL
