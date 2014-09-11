6793612cd23e1ae5a8fa0540212595a295c06faa
pawluczuk <pawluczuk.monika@gmail.com>
2014-09-11 14:29:45 +0200

    partial documentation and vignette

--- R/d3net-package.R
+++ R/d3net-package.R
@@ -1,8 +1,14 @@
-#' Visualizing networks using d3js and Shiny
+#' Visualizing networks using D3.js and Shiny
 #'
 #'Framework for creating Shiny applications for static and dynamic network
-#'visualizations using d3js	
+#'visualizations using D3.js JavaScript library (see more at: http://d3js.org).
+#'
+#'Visualisation is a force-directed layout interactive SVG graph, which nodes and edges 
+#'can be dragged and changed dynamically do reflect network properties. The graph itself is also 
+#'zoomable. For dynamic networks, player controls has been provided.
+#'
 #'
 #' @name d3net-package
+#' @author Michal Bojanowski, Monika Pawluczuk
 #' @docType package
 NULL
--- R/d3net.R
+++ R/d3net.R
@@ -1,8 +1,42 @@
 #' Launching Shiny apps
+#' 
+#' D3net application creates a visualisation of dynamic or static network. Graph layout has some
+#' predefined properties that can be changed through the controls in the app. 
+#' To launch the shiny app use functions described below. See Details for more information.
 #'
-#' Functions to launch the shiny app.
+#' D3net application creates a visualisation of dynamic or static network with some
+#' predefined properties. User may change these properties through 
+#' the available controls in 'd3 properties' section.
+#' 
+#' @section D3.js properties:
+#' Link distance, charge and vertex size are calculated depending on total number of nodes.
+#' To find out more about force-directed layout and its properties that was used for these 
+#' visualisations, 
+#' refer to https://github.com/mbostock/d3/wiki/Force-Layout.
+#' 
+#' @section R properties
+#' Vertices and edges properties of the visualised object can be reflected as well.
+#' Controls in 'R properties' section enable this.
+#' Logical and character network's vertices properties can be reflected by color nodes, 
+#' numeric ones by the size of the node. Edge properties can be reflected by edge thickness.
+#' All of the available properties may be added to the tooltip information, 
+#' which is shown on node mouseover.
+#' 
+#' @section Dynamic networks:
+#' For dynamic visualisations, user may also set the time interval between each timepoint. 
+#' It is by default 3000ms (3s). To play with the visualisation, use the controls below the graph.
+#' It is also possible to pause a visualisation and freeze the graph by pressing spacebar.
+#' 
+#' @section Large networks:
+#' For visualisations with large number of vertices, nodes size can be pretty small. 
+#' For this reason, there is a zooming control that lets you change the scale. 
+#' You can also move the whole graph in every direction. 
+#' As gestures may be interferring with the dragging, it's not possible to drag the 
+#' nodes when zooming is on.
+#' 
+#' @note Function transforms network object into igraph object using intergraph.
 #'
-#' @param dataset R object, see Details for available methods
+#' @param dataset R object, see Usage for available methods
 #'
 #' Currently methods are implemented for objects of class "igraph", "network",
 #' and "networkDynamic".
@@ -49,7 +83,6 @@ d3net.networkDynamic <- function(dataset)
   shiny::runApp( system.file("network-app", package="d3net") )
 }
 
-
 #' @method d3net network
 #' @rdname d3net
 #' @import intergraph
@@ -59,6 +92,7 @@ d3net.network <- function(dataset)
 {
   print("Running Shiny App for networks...")
   require(network)
+  require(intergraph)
   require(igraph)
   dataset <- asIgraph(dataset)
   .GlobalEnv$.d3net.dataset <- dataset
